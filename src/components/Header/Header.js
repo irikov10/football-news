1eb66a23e0051008d06545c0a39ba4cc7437d140
irikov10 <evgenijirikov10@gmail.com>
2023-04-01 21:25:10 +0300

    Authentication added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,73 @@
-import styles from './Header.module.css'
+import styles from "./Header.module.css";
 
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext } from "react";
 
 export default function Header() {
-    return (
-        <header className={styles["hero"]}>
-            <nav className={styles["navigation"]}>
-                <h1 className={styles["logo"]}>Football <span>News</span></h1>
-                <ul>
-                    <li><NavLink to="/"
-                        style={({ isActive }) => {
-                            return {
-                                color: isActive ? 'red' : '',
-                            };
-                        }}>Home</NavLink></li>
-                    <li><NavLink to="/dailyNews"
-                        style={({ isActive }) => {
-                            return {
-                                color: isActive ? 'red' : '',
-                            };
-                        }}>Everyday News</NavLink></li>
-                    <li><NavLink to="/transfers"
-                        style={({ isActive }) => {
-                            return {
-                                color: isActive ? 'red' : '',
-                            };
-                        }}>Top Transfers</NavLink></li>
-                </ul>
-                <NavLink to="/login" className={styles["sign-in-button"]}>Sign in</NavLink>
-            </nav>
-        </header>
-    )
+  const { isAuthenticated, userEmail } = useContext(AuthContext);
+
+  return (
+    <header className={styles["hero"]}>
+      <nav className={styles["navigation"]}>
+        <h1 className={styles["logo"]}>
+          Football <span>News</span>
+        </h1>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              style={({ isActive }) => {
+                return {
+                  color: isActive ? "red" : "",
+                };
+              }}
+            >
+              Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/dailyNews"
+              style={({ isActive }) => {
+                return {
+                  color: isActive ? "red" : "",
+                };
+              }}
+            >
+              Everyday News
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/transfers"
+              style={({ isActive }) => {
+                return {
+                  color: isActive ? "red" : "",
+                };
+              }}
+            >
+              Top Transfers
+            </NavLink>
+          </li>
+
+          {isAuthenticated ? (
+            <li>
+                <NavLink to="/logout" className={styles["logout-btn"]}>
+                    Logout
+                </NavLink>
+          </li>
+        ) : (
+            <li>
+                <NavLink to="/login" className={styles["sign-in-button"]}>
+                    Sign in
+                </NavLink>
+          </li>
+        )}
+        </ul>
+      </nav>
+    </header>
+  );
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,25 +5,58 @@ import { FaLock } from 'react-icons/fa'
 import background from '../../assets/images/overlay-pictures/football-ball.png'
 
 import { Link } from 'react-router-dom'
+import { useForm } from '../../hooks/useForm'
+import { useAuthContext } from '../../contexts/AuthContext'
 
+
+const loginFormKeys = {
+    email: 'email',
+    password: 'password',
+}
 
 export default function Login() {
+    const { onLoginSubmit } = useAuthContext();
+
+    const {values, changeHandler, onSubmit } = useForm({
+        [loginFormKeys.email]: '',
+        [loginFormKeys.password]: ''
+    }, onLoginSubmit)
+
     return (
         <main style={{ backgroundImage: `url(${background})` }}>
             <div className={styles["login-container"]}>
 
-                <form action="#" className={styles["login-form"]}>
+                <form 
+                    className={styles["login-form"]}
+                    id="login"
+                    method="POST"
+                    onSubmit={onSubmit}
+                >
                     <h2>Login</h2>
                     <div className={styles["input-box"]}>
                         <span className={styles["icon"]}><FaEnvelope /></span>
-                        <input type="email" required />
-                        <label>Email</label>
+                        <input 
+                            type="email" 
+                            id="email"
+                            name={loginFormKeys.email}
+                            value={values[loginFormKeys.email]}
+                            onChange={changeHandler}
+                            required
+                        />
+                        <label htmlFor="email">Email</label>
                     </div>
 
                     <div className={styles["input-box"]}>
                         <span className={styles["icon"]}><FaLock /></span>
-                        <input type="password" required />
-                        <label>Password</label>
+                        <input 
+                            id="password" 
+                            type="password"
+                            name={loginFormKeys.password}
+                            value={values[loginFormKeys.password]}
+                            onChange={changeHandler}
+                            required
+                        />
+                        <label htmlFor="password">Password</label>
                     </div>
 
                     <button type="submit" className={styles["login-btn"]}>Login</button>
