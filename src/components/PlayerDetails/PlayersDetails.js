0287b98f50fe8eb85736fa0a7c4d2d83d4f1ed56
irikov10 <evgenijirikov10@gmail.com>
2023-04-03 20:03:42 +0300

    PlayerDetails functionality made

--- src/components/PlayerDetails/PlayersDetails.js
+++ src/components/PlayerDetails/PlayersDetails.js
@@ -1,25 +1,34 @@
 import styles from './PlayersDetails.module.css';
-import enzo from '../../assets/player-images/player-details-enzo.jpg';
 import background from '../../assets/images/overlay-pictures/stadium.jpg'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import * as playersService from '../../services/playersService'
 
 export default function PlayersDetails() {
+    const [player, setPlayer] = useState([]);
+    console.log(player)
+    const { playerId } = useParams();
+
+    useEffect(() => {
+        playersService.getPlayerId(playerId)
+        .then(setPlayer)
+    }, [playerId])  
+
+    console.log(player)
+
     return (
         <main style={{backgroundImage: `url(${background})`}}>
             <div className={styles["player-details-image"]}>
-                <img src={enzo} alt="Enzo Fernandez" />
+                <img src={player.detailsImage} alt="" />
             </div>
 
             <div className={styles["player-details-content"]}>
-                <h1>Enzo Jeremías Fernández</h1>
-                <h2>Nationality: Argentinian</h2>
-                <h3>Born: 17 January 2001</h3>
+                <h1>{player.firstName} {player.middleName} {player.lastName}</h1>
+                <h2>Nationality: {player.nationality}</h2>
+                <h3>Born: {player.birthDate}</h3>
 
                 <p>
-                    Enzo Jeremias Fernandez is an is an Argentine professional footballer who plays as a central midfielder for Premier League club Chelsea and the Argentina national team.
-
-                    As an academy graduate of River Plate, Fernández made his first-team debut for the club in 2019, before spending two seasons on loan with Defensa y Justicia. There, he enjoyed a successful campaign which culminated in him winning the Copa Sudamericana and Recopa Sudamericana, before returning to River Plate in 2021. Following his return, Fernández established himself as an integral player for the club, and won the 2021 Argentine Primera División. He joined Portuguese Primeira Liga side Benfica in the summer of 2022. Having played only six months for Benfica, he was purchased by Premier League club Chelsea in January 2023 for a British-record transfer fee.
-
-                    An Argentine international, Fernández previously represented his country at under-18 level before making his senior international debut. He represented Argentina at the 2022 FIFA World Cup, playing an important role in helping his country win their third title, while also winning the tournament's Young Player Award.
+                    {player.description}
                 </p>
             </div>
         </main> 
