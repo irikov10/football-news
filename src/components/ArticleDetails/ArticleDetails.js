a7402124601012028e44da347bf596926fa4310b
irikov10 <evgenijirikov10@gmail.com>
2023-04-03 20:01:53 +0300

    ArticleDetails functionality made

--- src/components/ArticleDetails/ArticleDetails.js
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -6,9 +6,19 @@ import { AiOutlineLike } from 'react-icons/ai'
 import { IoMdArrowRoundBack } from 'react-icons/io'
 import { FaRegComment } from 'react-icons/fa'
 
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import * as newsService from '../../services/newsService'
+import { useEffect, useState } from 'react';
 
 export default function ArticleDetails() {
+    const [article, setArticle] = useState([]);
+    const { articleId } = useParams();
+
+    useEffect(() => {
+        newsService.getArticleId(articleId)
+        .then(setArticle)
+    }, [])
+
     return (
         <main className={styles["main"]}>
             <div className={styles["article-details"]}>
@@ -19,37 +29,30 @@ export default function ArticleDetails() {
 
                     <div className={styles["news-content"]}>
                         <div className={styles["news-author"]}>
-                            <img src={fabrizio} alt="Fabrizio Romano" className={styles["author-img"]} />
+                            <img src={article.image} alt="" className={styles["author-img"]} />
 
                             <div className={styles["author-information"]}>
-                                <h4 className={styles["author-news-name"]}>Fabrizio Romano</h4>
+                                <h4 className={styles["author-news-name"]}>{article.name}</h4>
 
                                 <div className={styles["sub-news-name"]}>
-                                    <h4>@FabrizioRomano</h4>
+                                    <h4>{article.subName}</h4>
                                 </div>
                             </div>
                         </div>
 
                         <div className={styles["article-news-content"]}>
                             <p className={styles["article-news-text"]}>
-                                Internal discussions are taking place at Tottenham on Antonio Conte situation with
-                                Daniel
-                                Levy
-                                to have final say — as Conte's plan to leave at the end of the season was already clear
-                                after
-                                UCL defeat 🚨⚪️ #THFC
-
-                                Conte's words were directed at the players — not board or owner.
+                                {article.content}
                             </p>
 
                             <div className={styles["article-details-image"]}>
-                                <img src={conte} alt="Antonio Conte" />
+                                <img src={article.articleImage} alt="" />
                             </div>
 
                             <hr className={styles["line"]} />
                             <div className={styles["article-details-interactions"]}>
-                                <p className={styles["like"]}><AiOutlineLike className={styles["icon"]} />30k</p>
-                                <p className={styles["comment"]}><FaRegComment className={styles["icon"]} />2000</p>
+                                <p className={styles["like"]}><AiOutlineLike className={styles["icon"]} /></p>
+                                <p className={styles["comment"]}><FaRegComment className={styles["icon"]} /></p>
                             </div>
                             <hr className={styles["line"]} />
                         </div>
