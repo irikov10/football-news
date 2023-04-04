a7b78202d8f3021880dd6429d9961d72d372791c
irikov10 <evgenijirikov10@gmail.com>
2023-04-04 21:25:29 +0300

    Updated newsService.js

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -1,16 +1,41 @@
 import { requesterFactory } from '../services/requester'
 
 const baseUrl = 'http://localhost:3030/jsonstore/football/'
-const request = requesterFactory();
 
-export const getAllArticles = async() => {
-    const result = await request.get(baseUrl + "news/");
+export const articlesServiceFactory = (token) => {
+    const request = requesterFactory(token);
 
-    return Object.values(result);
-}
+    const getAllArticles = async () => {
+        const result = await request.get(baseUrl + "news/");
 
-export const getArticleId = async(articleId) => {
-    const result = await request.get(baseUrl + `news/${articleId}`);
+        return Object.values(result);
+    }
 
-    return result
+    const getArticle = async (articleId) => {
+        const result = await request.get(baseUrl + `news/${articleId}`);
+
+        return result
+    }
+
+    const create = async (articleData) => {
+        const result = await request.post(baseUrl + `news/`, articleData);
+
+        return result
+    }
+
+    const edit = (articleId, data) => {
+        request.put(`${baseUrl}/news/${articleId}`, data);
+    }
+
+    const deleteArticle = (articleId) => {
+        request.delete(`${baseUrl}/news/${articleId}`);
+    }
+
+    return {
+        getAllArticles,
+        getArticle,
+        create,
+        edit,
+        delete: deleteArticle,
+    }
 }
