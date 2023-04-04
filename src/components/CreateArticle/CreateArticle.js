bb46ac5bc9876c2e7e88a97f82f915daed17fdbf
irikov10 <evgenijirikov10@gmail.com>
2023-04-04 21:26:44 +0300

    CreateArticle.js updated

--- src/components/CreateArticle/CreateArticle.js
+++ src/components/CreateArticle/CreateArticle.js
@@ -1,28 +1,79 @@
 import styles from './CreateArticle.module.css'
+import { useArticlesContext } from '../../contexts/ArticleContext'
+import { useForm } from '../../hooks/useForm'
 
-export default function CreateArticle() {
+export const CreateArticle = () => {
+
+    const { onCreateArticleSubmit } = useArticlesContext();
+
+    const { values, changeHandler, onSubmit } = useForm(
+        {
+            name: '',
+            subName: '',
+            content: '',
+            articleImage: '',
+        }, onCreateArticleSubmit
+    )
     return (
-        <body>
-            <div className={styles["create-article"]}>
+        <div className={styles["create-article"]}>
 
-                <h1>Create Article</h1>
+            <h1>Create Article</h1>
 
-                <form action="#" className={styles["create-form"]}>
-                    <div className={styles["input-box"]}>
-                        <label>Content:</label>
-                        <input type="text" name="content" id={styles["content"]} />
-                    </div>
+            <form
+                className={styles["create-form"]}
+                id="createForm"
+                method="POST"
+                onSubmit={onSubmit}
+            >
+                <div className={styles["input-box"]}>
+                    <label>Name: </label>
+                    <input
+                        type="text"
+                        name="name"
+                        id={styles["content"]}
+                        values={values.name}
+                        onChange={changeHandler}
+                    />
+                </div>
 
-                    <div className={styles["input-box"]}>
-                        <label>Image URL:</label>
-                        <input type="text" name="imgURL" id={styles["article-image"]} />
-                    </div>
+                <div className={styles["input-box"]}>
+                    <label>Subname: </label>
+                    <input
+                        type="text"
+                        name="subName"
+                        id={styles["content"]}
+                        values={values.subName}
+                        onChange={changeHandler}
+                    />
+                </div>
 
-                    <div className={styles["create-post-button"]}>
-                        <a href="#">Post</a>
-                    </div>
-                </form>
-            </div>
-        </body>
+
+                <div className={styles["input-box"]}>
+                    <label>Content: </label>
+                    <input
+                        type="text"
+                        name="content"
+                        id={styles["content"]}
+                        values={values.content}
+                        onChange={changeHandler}
+                    />
+                </div>
+
+                <div className={styles["input-box"]}>
+                    <label>Image URL:</label>
+                    <input
+                        type="text"
+                        name="articleImage"
+                        id={styles["article-image"]}
+                        values={values.articleImage}
+                        onChange={changeHandler}
+                    />
+                </div>
+
+                <div className={styles["create-post-button"]}>
+                    <button type="submit" value="Create">Post</button>
+                </div>
+            </form>
+        </div>
     )
 }
