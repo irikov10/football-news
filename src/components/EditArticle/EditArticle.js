8b197a8aa0b377cc059f2deb998917657205a1e9
irikov10 <evgenijirikov10@gmail.com>
2023-04-04 21:27:31 +0300

    Updated EditArticle and Edit css

--- src/components/EditArticle/EditArticle.js
+++ src/components/EditArticle/EditArticle.js
@@ -1,28 +1,92 @@
 import styles from './Edit.module.css'
 
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useArticlesContext } from '../../contexts/ArticleContext';
+import { useForm } from '../../hooks/useForm';
+import { useService } from '../../hooks/useService';
+import { articlesServiceFactory } from '../../services/newsService';
+
 export default function EditArticle() {
+
+    const { onArticleEditSubmit } = useArticlesContext();
+    const { articleId } = useParams();
+    const articlesFactory = useService(articlesServiceFactory);
+    const { values, changeHandler, onSubmit, changeValues } = useForm(
+        {
+            name: '',
+            subName: '',
+            content: '',
+            articleImage: '',
+        },
+        onArticleEditSubmit
+    );
+
+    useEffect(() => {
+        articlesFactory.getArticle(articleId)
+            .then((data) => {
+                changeValues(data);
+            });
+    }, [articleId]);
+
     return (
-        <body>
-            <div className={styles["edit-article"]}>
+        <div className={styles["edit-article"]}>
 
-                <h1>Edit Article</h1>
+            <h1>Edit Article</h1>
 
-                <form action="#" className={styles["edit-form"]}>
-                    <div className={styles["input-box"]}>
-                        <label>Content:</label>
-                        <input type="text" name="content" id={styles["content"]} />
-                    </div>
+            <form
+                className={styles["edit-form"]}
+                onSubmit={onSubmit}
+                method="POST"
+            >
 
-                    <div className={styles["input-box"]}>
-                        <label>Image URL:</label>
-                        <input type="text" name="imgURL" id={styles["article-image"]} />
-                    </div>
+                <div className={styles["input-box"]}>
+                    <label>Name:</label>
+                    <input type="text"
+                        name="name"
+                        id={styles["article-image"]}
+                        onChange={changeHandler}
+                        value={values.name}
+                    />
+                </div>
 
-                    <div className={styles["create-edit-button"]}>
-                        <a href="#">Edit</a>
-                    </div>
-                </form>
-            </div>
-        </body>
+                <div className={styles["input-box"]}>
+                    <label>SubName:</label>
+                    <input
+                        type="text"
+                        name="subName"
+                        id={styles["article-image"]}
+                        onChange={changeHandler}
+                        value={values.subName}
+                    />
+                </div>
+
+                <div className={styles["input-box"]}>
+                    <label>Content:</label>
+                    <input
+                        type="text"
+                        name="content"
+                        id={styles["content"]}
+                        onChange={changeHandler}
+                        value={values.content}
+                    />
+                </div>
+
+                <div className={styles["input-box"]}>
+                    <label>Image URL:</label>
+                    <input
+                        type="text"
+                        name="articleImage"
+                        id={styles["article-image"]}
+                        onChange={changeHandler}
+                        value={values.articleImage}
+                    />
+                </div>
+
+                <div className={styles["create-edit-button"]}>
+                    <input type="submit">Edit</input>
+                </div>
+            </form>
+        </div>
     )
 }
