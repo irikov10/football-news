4c09636435747e6419b5f44eaed804057a1f667b
irikov10 <evgenijirikov10@gmail.com>
2023-04-04 21:26:01 +0300

    AuthContext.js updated

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -6,72 +6,72 @@ import { authServiceFactory } from "../services/authService";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useLocalStorage("auth", {});
-  const navigate = useNavigate();
-  const authService = authServiceFactory(auth.accessToken);
+	const [auth, setAuth] = useLocalStorage("auth", {});
+	const navigate = useNavigate();
+	const authService = authServiceFactory(auth.accessToken);
 
-  const onLoginSubmit = async (data) => {
-    try {
-      const result = await authService.login(data);
+	const onLoginSubmit = async (data) => {
+		try {
+			const result = await authService.login(data);
 
-      setAuth(result);
+			setAuth(result);
 
-      navigate("/");
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+			navigate("/");
+		} catch (error) {
+			alert(error.message);
+		}
+	};
 
-  const onRegisterSubmit = async (values) => {
-    const { repeatPassword, ...registerData } = values;
+	const onRegisterSubmit = async (values) => {
+		const { repeatPassword, ...registerData } = values;
 
-    console.log(values);
+		console.log(values);
 
-    if (!repeatPassword || !registerData) {
-      throw new Error("All fields are required");
-    }
+		if (!repeatPassword || !registerData) {
+			throw new Error("All fields are required");
+		}
 
-    if (repeatPassword !== registerData.password) {
-      throw new Error("Passwords do not match");
-    }
+		if (repeatPassword !== registerData.password) {
+			throw new Error("Passwords do not match");
+		}
 
-    try {
-      const result = await authService.register(registerData);
+		try {
+			const result = await authService.register(registerData);
 
-      setAuth(result);
+			setAuth(result);
 
-      navigate("/");
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+			navigate("/");
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
 
-  const onLogout = async () => {
-    await authService.logout();
+	const onLogout = async () => {
+		await authService.logout();
 
-    setAuth({});
-  };
+		setAuth({});
+	};
 
-  const contextValues = {
-    onLoginSubmit,
-    onRegisterSubmit,
-    onLogout,
-    userId: auth._id,
-    token: auth.accessToken,
-    userEmail: auth.email,
-    isAuthenticated: !!auth.accessToken,
-  };
+	const contextValues = {
+		onLoginSubmit,
+		onRegisterSubmit,
+		onLogout,
+		userId: auth._id,
+		token: auth.accessToken,
+		userEmail: auth.email,
+		isAuthenticated: !!auth.accessToken,
+	};
 
-  return (
-    <>
-      <AuthContext.Provider value={contextValues}>
-        {children}
-      </AuthContext.Provider>
-    </>
-  );
+	return (
+		<>
+			<AuthContext.Provider value={contextValues}>
+				{children}
+			</AuthContext.Provider>
+		</>
+	);
 };
 
 export const useAuthContext = () => {
-  const context = useContext(AuthContext);
-  return context;
+	const context = useContext(AuthContext);
+	return context;
 };
